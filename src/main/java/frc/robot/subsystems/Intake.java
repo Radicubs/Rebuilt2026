acdab724cfaf7b7cadc515f12033a9c2cffd7569
Vihan Katikar <vihankatikar@Vihans-MacBook-Pro.local>
2026-02-25 20:16:32 -0600

    added pid and feedforward to intake code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -14,6 +17,13 @@ public class Intake extends SubsystemBase {
     private static Intake INSTANCE;
 
     private SparkMax intakeMotor;
+
+    public double targetVelocity;
+    private boolean pidEnabled = false;
+
+    private final PIDController controller;
+    private final SimpleMotorFeedforward feedforward;
+    private final RelativeEncoder encoder;
 
     public static Intake getInstance(){
         if(INSTANCE == null) {INSTANCE = new Intake();}
@@ -26,11 +36,36 @@ public class Intake extends SubsystemBase {
         intakeConfig.inverted(true);
         intakeConfig.idleMode(SparkBaseConfig.IdleMode.kCoast);
         intakeMotor.configure(intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        encoder = intakeMotor.getEncoder();
+        controller = new PIDController(Constants.Intake.PIDFeedforwardConstants.P, Constants.Intake.PIDFeedforwardConstants.I, Constants.Intake.PIDFeedforwardConstants.D);
+        controller.setTolerance(0.05);
+
+        feedforward = new SimpleMotorFeedforward(Constants.Intake.PIDFeedforwardConstants.S, Constants.Intake.PIDFeedforwardConstants.V);
     }
 
     public void setIntakeSpeed(double speed){
         intakeMotor.set(speed);
     }
+    public void reset() { intakeMotor.set(0); }
+    public double getVelocity() { return encoder.getVelocity(); }
+    public double getSetpoint() { return controller.getSetpoint(); }
+    public void setSetpoint(double targetVelocity) {
+        reset();
+        pidEnabled = true;
+        this.targetVelocity = targetVelocity;
+        controller.setSetpoint(targetVelocity);
+    }
+    public void cancelPID() {
+        pidEnabled = false;
+    }
 
-
+    @Override
+    public void periodic() {
+        if (pidEnabled) {
+            double motorSpeed = controller.calculate(getVelocity());
+            double ffValue = feedforward.calculate(getVelocity());
+            intakeMotor.set(motorSpeed - ffValue);
+        }
+    }
 }
